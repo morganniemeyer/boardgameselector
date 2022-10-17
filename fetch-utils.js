e570b9bd8d966154ccf6adb333b99a3043c7fa29
Sam Adams <sam.adams7785@gmail.com>
2022-10-17 12:30:51 -0700

    profile form eventlistener

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,5 +1,6 @@
 const SUPABASE_URL = 'https://xxthqkssauqxbvfqvxjw.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4dGhxa3NzYXVxeGJ2ZnF2eGp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjU3Njk2NzgsImV4cCI6MTk4MTM0NTY3OH0.Q87tH-nJiXpzJshzZkJhpTtywC-WdXx2KAOgieO-VT8';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4dGhxa3NzYXVxeGJ2ZnF2eGp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjU3Njk2NzgsImV4cCI6MTk4MTM0NTY3OH0.Q87tH-nJiXpzJshzZkJhpTtywC-WdXx2KAOgieO-VT8';
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 /* Auth related functions */
@@ -27,3 +28,23 @@ export async function signOutUser() {
 }
 
 /* Data functions */
+
+export async function uploadImage(bucketName, imagePath, imageFile) {
+    const bucket = client.storage.from(bucketName);
+    const response = await bucket.upload(imagePath, imageFile, {
+        cacheControl: '3600',
+        upsert: true,
+    });
+
+    if (response.error) {
+        return null;
+    }
+
+    const url = `${SUPABASE_URL}/storage/v1/object/public/${response.data.Key}`;
+
+    return url;
+}
+
+export async function upsertProfile(profile) {
+    return await client.from('profiles').upsert(profile).single().eq('user_id', profile.user_id);
+}
--- upsert-profile/upsert-profile.js
+++ upsert-profile/upsert-profile.js
@@ -1,15 +1,16 @@
 /* Imports */
 // this will check if we have a user and set signout link if it exists
 import '../auth/user.js';
+import { uploadImage, upsertProfile } from '../fetch-utils.js';
 
 /* Get DOM Elements */
 const preview = document.getElementById('image-preview');
 const errorDisplay = document.getElementById('error-display');
 const imageInput = document.getElementById('image-input');
+const form = document.querySelector('form');
 
 /* State */
 let error = null;
-let profile = null;
 
 /* Events */
 imageInput.addEventListener('change', () => {
@@ -18,6 +19,36 @@ imageInput.addEventListener('change', () => {
         preview.src = URL.createObjectURL(avatar);
     } else {
         preview.src = '/assets/default.jpg';
+    }
+});
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    let url = null;
+    const imageFile = formData.get('form');
+
+    if (imageFile) {
+        const randomFolder = Math.floor(Date.now() * Math.random());
+        const imagePath = `post/${randomFolder}/${imageFile.name}`;
+        url = await uploadImage('bucket1', imagePath, imageFile);
+    }
+
+    const profile = {
+        user_name: formData.get('user_name'),
+        bio: formData.get('bio'),
+        avatar: url,
+        favorite: formData.get('favorite'),
+    };
+
+    const response = await upsertProfile(profile);
+    error = response.error;
+
+    if (error) {
+        displayError();
+    } else {
+        location.assign('../profile/index.html');
     }
 });
 
